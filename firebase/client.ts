3cfc2ea43c2cd00ff99f047ab03f7dfbfe487ebf
AxAbhishek0309 <auxinbiotek1986@gmail.com>
2025-04-04 01:52:21 +0530

    Adding project files

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,26 +1,33 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics"; 
+
+import { getAuth } from "firebase/auth"; 
+import { getFirestore } from "firebase/firestore"; 
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyChx6CH--5OrdXsD4TCGkt2k7YhQwkHn6E",
+  authDomain: "prepwise-90226.firebaseapp.com",
+  projectId: "prepwise-90226",
+  storageBucket: "prepwise-90226.firebasestorage.app",
+  messagingSenderId: "352718115661",
+  appId: "1:352718115661:web:d53e42beda14ca7e605d04",
+  measurementId: "G-7D401FYDX0"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
+const app = !getApps.length ?  initializeApp(firebaseConfig) : getApp() ; 
+export const auth = getAuth(app); 
+export const db = getFirestore(app); 
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// const analytics = getAnalytics(app); 
